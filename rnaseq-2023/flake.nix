86cfb7dcb79e99dd003386eb37560d37e2ddda42
AJ Fagan <ajfagan@wisc.edu>
2024-09-09 13:01:40 -0500

    direnv work

--- rnaseq-2023/flake.nix
+++ rnaseq-2023/flake.nix
@@ -14,6 +14,7 @@
         devShells.default = pkgs.mkShell { 
 	  packages = with pkgs; [ 
 	    bashInteractive
+	    jdk11
 	    (rstudioWrapper.override {
 	      packages = with pkgs.rPackages; 
 	      let allez = buildRPackage {
@@ -21,23 +22,32 @@
 		src = fetchFromGitHub {
 		  owner = "wiscstatman";
 		  repo = "allez";
-		  revision = "3f9f40acfd5b85ae9f6b599eb67d2fd987030a76";
+		  rev = "3f9f40acfd5b85ae9f6b599eb67d2fd987030a76";
 		  hash =  "sha256-6tQ3chYHCPDkLg1LllenETptvolXtPcF6XWrg47orno=";
 		};
+		propogatedBuildInputs = [ rlang AnnotationDbi ];
+		nativeBuildInputs = [ rlang AnnotationDbi ];
 	      };
 	      in 
-	      [
-		ggplot2
-		dplyr
-		org_Hs_eg_db
-		DESeq2
-		pheatmap
-		RColorBrewer
-		pathfindR
-		allez
-	      ];
+		[
+		  ggplot2
+		  dplyr
+		  org_Hs_eg_db
+		  DESeq2
+		  pheatmap
+		  RColorBrewer
+		  allez
+		  rmarkdown
+		  knitr
+		  pathfindR
+		];
 	    })
 	  ]; 
+	  buildInputs = with pkgs; [ jdk11 ];
+	  shellHook = ''
+	    export JAVA_HOME=${pkgs.jdk11}
+	    PATH="${pkgs.jdk11}/bin:$PATH"
+	  '';
 	};
       }
     );
